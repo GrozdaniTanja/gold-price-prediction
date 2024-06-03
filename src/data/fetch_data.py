11e534a76ab97d19986bd0102ca828afc2e7504b
Tanja <tanja.grozdani@student.um.si>
2024-06-03 18:43:53 +0200

    change apikey

--- src/data/fetch_data.py
+++ src/data/fetch_data.py
@@ -10,7 +10,7 @@ RAW_DATA_DIR = os.path.join(DATA_DIR, "raw")
 os.makedirs(DATA_DIR, exist_ok=True)
 os.makedirs(RAW_DATA_DIR, exist_ok=True)
 
-API_KEY = "goldapi-3hja19lwj1d0v0-io"
+API_KEY = "goldapi-acjnanslwz78x7d-io"
 SYMBOL = "XAU"
 CURRENCY = "EUR"
 
